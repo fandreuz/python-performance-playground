74d9cdc568887e9dd7a871d832d45c0fca24d956
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-07-24 14:57:48 +0200

    increase control over plots

--- plot_machinery/plot.py
+++ plot_machinery/plot.py
@@ -1,5 +1,6 @@
 import perfplot
 import numpy as np
+import matplotlib.pyplot as plt
 
 _data_func = None
 _kernels = []
@@ -33,12 +34,20 @@ def kernel(label):
     return decorator_kernel
 
 
-def plot(target_time_per_measurement=1.e-2):
-    perfplot.show(
+def plot(
+    target_time_per_measurement=1.0e-2, xlabel=None, logx="auto", logy="auto"
+):
+    plt.figure(figsize=(20, 8))
+    perfplot.plot(
         setup=_data_func,
         kernels=_kernels,
         labels=_kernel_labels,
         n_range=_steps,
-        xlabel="n",
+        xlabel="n" if xlabel is None else xlabel,
         target_time_per_measurement=target_time_per_measurement,
+        show_progress=False,
+        logx=logx,
+        logy=logy,
     )
+    plt.grid()
+    plt.show()
