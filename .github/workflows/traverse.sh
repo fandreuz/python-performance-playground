47988020a49f2a1fda19b7876f47069763517504
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-07-24 02:33:00 +0200

    minor fixes

--- .github/workflows/traverse.sh
+++ .github/workflows/traverse.sh
@@ -4,8 +4,8 @@ function traverse() {
 for file in "$1"/*
 do
     if [ ! -d "${file}" ] ; then
-        if [ "${file: -6}" == ".ipynb" ]
-            jupyter nbconvert --to md $file
+        if [ "${file: -6}" == ".ipynb" ] ; then
+            jupyter nbconvert --to markdown "$file"
         fi
     else
         traverse "${file}"
